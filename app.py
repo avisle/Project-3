c5acb4e878abcb40e3b58716a6ba9729a77bbc29
bguzhu <zhubrendan@gmail.com>
2023-11-29 21:39:16 -0800

    Include plots.js and dropdown for interactive visualization

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 from flask import Flask, render_template, jsonify, request
-from config1819 import username, password, hostname, port, db
+from config import username, password, hostname, port, db
 import pandas as pd
 
 
@@ -12,31 +12,35 @@ engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{hostname}:
 
 # Loop through all tables to get data from SQL to API
 @app.route("/api/v1.0/flu_data")
-def flu(countyIn):
-    #table = ["fluvacc19", "fluvacc20", "fluvacc21", "fluvacc22"]
+def flu():
+    table = ["fluvacc19", "fluvacc20", "fluvacc21", "fluvacc22"]
     df = []
-    #for i in table:
-        #query = f"SELECT * FROM {i}"
-    query = f"SELECT * FROM fluvacc_all WHERE county = {countyIn}"
-    conn = engine.connect()
-    season = pd.read_sql(text(query), conn)
-    df.append(season)
-    conn.close()
+    for i in table:
+        query = f"SELECT * FROM {i}"
+        conn = engine.connect()
+        season = pd.read_sql(text(query), conn)
+        df.append(season)
+        conn.close()
     combined_df = pd.concat(df, ignore_index=True)
     return combined_df.to_json(orient="records")
 
+# Get all the unique counties for the dropdown menu
 @app.route("/api/v1.0/counties")
 def counties():
-    #table = ["fluvacc19", "fluvacc20", "fluvacc21", "fluvacc22"]
-    df = []
-    #for i in table:
     query = "SELECT distinct county FROM fluvacc_all ORDER BY county"
     conn = engine.connect()
-    season = pd.read_sql(text(query), conn)
-    df.append(season)
+    df = pd.read_sql(text(query), conn)
     conn.close()
-    combined_df = pd.concat(df, ignore_index=False)
-    return combined_df.to_json(orient="records")
+    return df.to_json(orient="records")
+
+# Create a route using a filter for counties based on user dropdown selection
+@app.route("/api/v1.0/flu_data/<county>")
+def county(county):
+    query = f"SELECT flu_season, percentage_healthcare_professionals_vaccinated FROM fluvacc_all WHERE county = '{county}'"
+    conn = engine.connect()
+    df = pd.read_sql(text(query), conn)
+    conn.close()
+    return df.to_json(orient="records")
 
 @app.route("/")
 def home():
@@ -45,25 +49,8 @@ def home():
 
 @app.route("/dashboard")
 def dashboard():
-    # TO DO - get data from database
-    # TO DO - pass data  to clean using sql query 
-    # CONVERT TO A BUNCH OF LISTS 
-
-    # SEND TO DASHBOARD.HTML
-    data = {
-        "yaxis" : [0,10,20,30],
-        "xaxis" : ["2018-2019", "2020-2021", "2021-2022", "2022-2023"],
-    }
 
     return render_template("dashboard.html")
-
-
-# @app.route("/api/v1.0/sumpetals")
-# def sumpetaliris():
-#     conn = engine.connect()
-#     query = "SELECT SUM(petalwidthcm) as petalwidth, SUM(petallengthcm) as petallength FROM iris"
-#     df = pd.read_sql(query, conn)
-#     return df.to_json(orient="records")
 
 
 if __name__ == "__main__":
--- static/plots.js
+++ static/plots.js
@@ -1,18 +1,41 @@
 
-function init(){
-    
-        var trace1 = {
-            x: xData,
-            y: yData
-        };
 
-        var data = [trace1];
-
-        var layout = {
-            title: "County % Vacc Vs Season"
+function optionChanged(countyName){
+    let selector = d3.select("countyDropDown");
+    var url = (`http://127.0.0.1:5000/api/v1.0/flu_data/${countyName}`);
+    d3.json(url).then(function(data) {
+        let fluseason = []
+        let vacc = []
+        for (i = 0 ; i< data.length; i++){
+            fluseason.push(data[i].flu_season)
+            vacc.push(data[i].percentage_healthcare_professionals_vaccinated)
+         }
+        var trace = {
+            x: fluseason,
+            y: vacc,
+            type: 'line'
         }
 
-        Plotly.newPlot("plot", data, layout);
+        var data = [trace];
+
+        var layout = {
+            title: "County Healthcare Professionals % Vacc Vs Season",
+            xaxis: {
+                title: 'Flu Season'
+            },
+            yaxis: {
+                title: 'Healthcare Professionals % Vaccinated'
+                }
+            }
+
+            Plotly.newPlot("plot", data, layout);
+     })
 }
 
-init();
+//get the selected county value in the DropDown options
+var dropDown=document.getElementById("countyDropDown");
+var selectedCounty=dropDown.selectedCounty;
+
+optionChanged(selectedCounty);
+
+
